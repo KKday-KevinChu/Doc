1415636cbf67db0605219c325d35878d0fead78f
Kevin <kevin.chu@kkday.com>
2026-06-09 04:50:27 +0800

    upd

--- index_files/fcm.js
+++ index_files/fcm.js
@@ -30,35 +30,98 @@ if (firebase.messaging.isSupported()) {
                 event_action: data.event_action
             };
 
-            var open_status = false;
-            var notification = new Notification(notificationTitle, notificationOptions);
-            notification.onclick = function(e) {
-                e.preventDefault();
-                window.open(data.click_action, '_blank');
-                fetch(data.ga_click, {method: 'get'});
-                if (data.event_action) {
-                    console.log('FCM_click');
-                    fetch(data.event_action + '&action=open', {method: 'get'});
-                }
-                open_status = true;
-                notification.close();
-            }
+            // GA4 收集事件追蹤
+            const ga_measurement_id = data.ga_measurement_id;
+            const ga_collect_url = data.ga_collection;
+            const ga_event = data.ga_event_name;
+            const ga_category = data.ga_category;
+            const ga_label = data.ga_label;
+            const ga_debug = data.ga_debug;
+            const gaPayload = {
+                client_id: '',
+                events: [{
+                    name: ga_event,
+                    params: {
+                        event_action: "received",
+                        event_category: ga_category,
+                        event_label: ga_label,
+                        debug_mode: ga_debug
+                    }
+                }]
+            };
 
-            notification.onclose = function(e) {
-                if (open_status == false) {
+            try {
+                var open_status = false;
+                var notification = new Notification(notificationTitle, notificationOptions);
+                notification.onclick = function(e) {
                     e.preventDefault();
-                    fetch(data.ga_close, {method: 'get'});
+                    window.open(data.click_action, '_blank');
+                    if (typeof(ga_collect_url) != 'undefined' && typeof(ga_measurement_id) != 'undefined' && ga_collect_url != '' && ga_measurement_id != '') {
+                        gtag('get', ga_measurement_id, 'client_id', (clientID) => {
+                            gaPayload.client_id = clientID;
+                            gaPayload.events[0].params.event_action = 'click';
+                            fetch(ga_collect_url, {
+                                method: 'POST',
+                                body: JSON.stringify(gaPayload),
+                            }).then(response => {
+                            }).catch(error => {
+                            });
+                        });
+                    }
                     if (data.event_action) {
-                        console.log('FCM_close');
-                        fetch(data.event_action + '&action=close', {method: 'get'});
+                        console.log('FCM_click');
+                        fetch(data.event_action + '&action=open', {method: 'get'});
+                    }
+                    open_status = true;
+                    notification.close();
+                }
+
+                notification.onclose = function(e) {
+                    if (open_status == false) {
+                        e.preventDefault();
+                        if (typeof(ga_collect_url) != 'undefined' && typeof(ga_measurement_id) != 'undefined' && ga_collect_url != '' && ga_measurement_id != '') {
+                            gtag('get', ga_measurement_id, 'client_id', (clientID) => {
+                                gaPayload.client_id = clientID;
+                                gaPayload.events[0].params.event_action = 'close';
+                                fetch(ga_collect_url, {
+                                    method: 'POST',
+                                    body: JSON.stringify(gaPayload)
+                                }).then(response => {
+                                }).catch(error => {
+                                });
+                            });
+                        }
+                        if (data.event_action) {
+                            console.log('FCM_close');
+                            fetch(data.event_action + '&action=close', {method: 'get'});
+                        }
                     }
                 }
-            }
 
-            fetch(data.ga_received, { method: 'get' });
-            if (data.event_action) {
-                console.log('FCM_received');
-                fetch(data.event_action + '&action=received', {method: 'get'});
+                try {
+                    console.log('aa'+ga_collect_url);
+                    if (typeof(ga_collect_url) != 'undefined' && typeof(ga_measurement_id) != 'undefined' && ga_collect_url != '' && ga_measurement_id != '') {
+                        gtag('get', ga_measurement_id, 'client_id', (clientID) => {
+                            gaPayload.client_id = clientID;
+                            console.log('fff');
+                            fetch(ga_collect_url, {
+                                method: 'POST',
+                                body: JSON.stringify(gaPayload)
+                            }).then(response => {
+                            }).catch(error => {
+                            });
+                        });
+                    }
+                } catch (error) {
+                    console.log('GA4 fetch failed:' + error);
+                }
+
+                if (data.event_action) {
+                    console.log('FCM_received');
+                    fetch(data.event_action + '&action=received', {method: 'get'});
+                }
+            } catch (error) {
+                console.log('Notification error: ' + error);
             }
         }
     });
